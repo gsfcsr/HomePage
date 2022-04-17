82936175c677c364d41071313b01ec03b8c603c7
gsfcsr <43461992+gsfcsr@users.noreply.github.com>
2022-04-17 20:34:19 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -126,9 +126,9 @@ require(['jquery'], function($) {
 				logoUrl = this.get('logo');
 			} else {
 				if (this.get('nightMode')) {
-					logoUrl = "logo/Logo_google.png";
+					logoUrl = "logo/Logo_2022.png";
 				} else {
-					logoUrl = "logo/Logo_google.png";
+					logoUrl = "logo/Logo_2022.png";
 				}
 			}
 			if (this.get('LogoHeightSet')) {
