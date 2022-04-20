1eca6626e8e13caadf9f029bc92b05d23d79edf8
gsfcsr <43461992+gsfcsr@users.noreply.github.com>
2022-04-20 21:58:10 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -126,7 +126,7 @@ require(['jquery'], function($) {
 				logoUrl = this.get('logo');
 			} else {
 				if (this.get('nightMode')) {
-					logoUrl = "logo/Logo_2022_N.png";
+					logoUrl = "logo/Logo_2022.png";
 				} else {
 					logoUrl = "logo/Logo_2022.png";
 				}
